2e03d69702874be1ce0099099d4a435c8d40d8f7
Felipe Alves <115760676+felipenewplayer@users.noreply.github.com>
2024-09-29 00:10:42 -0300

    H2 Database, test profile, JPA

--- src/main/java/com/estudandonaweb/course/entities/User.java
+++ src/main/java/com/estudandonaweb/course/entities/User.java
@@ -3,9 +3,21 @@ package com.estudandonaweb.course.entities;
 import java.io.Serializable;
 import java.util.Objects;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+
+@Entity
+@Table(name = "tb_user")
 public class User implements Serializable {
 	private static final long serialVersionUID = 1L;
-	private Integer id;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 	private String name;
 	private String email;
 	private String phone;
@@ -14,8 +26,8 @@ public class User implements Serializable {
 	public User () {
 		
 	}
-
-	public User(Integer id, String name, String email, String phone, String password) {
+	
+	public User(Long id, String name, String email, String phone, String password) {
 		this.id = id;
 		this.name = name;
 		this.email = email;
@@ -23,11 +35,11 @@ public class User implements Serializable {
 		this.password = password;
 	}
 
-	public Integer getId() {
+	public Long getId() {
 		return id;
 	}
 
-	public void setId(Integer id) {
+	public void setId(Long id) {
 		this.id = id;
 	}
 
