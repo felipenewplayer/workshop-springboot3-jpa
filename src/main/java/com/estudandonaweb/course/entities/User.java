73e852d82f5cdf687f79852a20cadee8e54ef88b
Felipe Alves <115760676+felipenewplayer@users.noreply.github.com>
2024-09-29 16:40:55 -0300

    Category

--- src/main/java/com/estudandonaweb/course/entities/User.java
+++ src/main/java/com/estudandonaweb/course/entities/User.java
@@ -14,12 +14,11 @@ import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
-
 @Entity
 @Table(name = "tb_user")
 public class User implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
@@ -27,19 +26,19 @@ public class User implements Serializable {
 	private String email;
 	private String phone;
 	private String password;
-	
+
 	@JsonIgnore
 	@OneToMany(mappedBy = "client")
 	private List<Order> orders = new ArrayList<>();
-	
+
 	public List<Order> getOrders() {
 		return orders;
 	}
-	
-	public User () {
-		
+
+	public User() {
+
 	}
-	
+
 	public User(Long id, String name, String email, String phone, String password) {
 		this.id = id;
 		this.name = name;
@@ -104,7 +103,5 @@ public class User implements Serializable {
 		User other = (User) obj;
 		return Objects.equals(id, other.id);
 	}
-	
-	
-	
+
 }
---
+++ src/main/java/com/estudandonaweb/course/repositories/CategoryRepository.java
@@ -0,0 +1,8 @@
+package com.estudandonaweb.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepository extends JpaRepository<com.estudandonaweb.course.entities.Category, Long> {
+
+	
+}
